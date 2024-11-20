5980a0251ee4ed040c5069ad90698a491a59a9e2
ZFooox <113981639+ZFooox@users.noreply.github.com>
2024-11-20 17:55:57 +0100

    start: first files

---
+++ get_next_line.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jocroon <jocroon@student.s19.be>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/20 17:38:00 by jocroon           #+#    #+#             */
+/*   Updated: 2024/11/20 17:46:24 by jocroon          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+//main function
+char	*get_next_line(int fd);
+
+//utils function
+
+#endif
